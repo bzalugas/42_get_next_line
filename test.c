885dbc0a1ef95ba61531b1a00ea19459f7bf762a
bazaluga <bazaluga@student.42.fr>
2024-02-21 18:47:24 +0100

    Change test

--- test.c
+++ test.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 #include "get_next_line.h"
 #include <fcntl.h>
+#include <string.h>
 int main()
 {
 	char *l;
 
-	int fd = open("text.txt", O_CREAT, O_RDONLY);
+	int fd = open("quran.txt", O_RDONLY);
 	l = get_next_line(fd);
 	while (l)
 	{
-		printf("text : %s", l);
+		fputs(l, stdout);
 		free(l);
 		l = get_next_line(fd);
 	}
-
+	close(fd);
 	return 0;
 }
